60fd0b5b166585c7f298430d55b594f189866b08
Nimishan <nimishan.siv@gmail.com>
2015-07-10 22:23:04 +0530

    class not found

---
+++ codes/src/loop.java
@@ -0,0 +1,13 @@
+/**
+ * Created by nimishan on 10/07/2015.
+ */
+import java.util.*;
+public class loop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
